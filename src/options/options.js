242f6ea730dda1472aae9906c2f5e9c38fa6ca36
noccu <37862870+noccu@users.noreply.github.com>
2024-04-19 10:55:00 +0200

    Properly implement options load/save

--- src/options/options.js
+++ src/options/options.js
@@ -6,18 +6,30 @@ var DBG_LAST_LIST_DL
 var TOTAL_SUBS = 0
 
 
+export async function loadOptions() {
+    let savedCfg = await chrome.storage.local.get(CONFIG_DEFAULT)
+    console.debug(savedCfg)
+    return savedCfg
+    // savedCfg.lists.forEach(uiAddList);
+};
+
+export function saveOptions (cfg) {
+    console.log("Saving config")
+    // fullCfg = structuredClone(CONFIG_DEFAULT)
+    // Object.assign(fullCfg, cfg)
+    chrome.storage.local.set(cfg);
+    //todo: send update event
+    console.log("Config saved")
+    setStatus("Config saved")
+  };
+
+
+// UI background
 function fetchNewList(ev) {
     if (ev.key != "Enter" || !ev.target.checkValidity()) return
     // ev.preventDefault()
     downLoadList(ev.target.value).then(uiAddList)
 }
-
-function loadOptions(savedCfg) {
-    dbgLog(savedCfg)
-    savedCfg.lists.forEach(l => {
-        uiAddList(l)
-    });
-};
 
 
 // UI background
@@ -38,19 +50,6 @@ function setStatus(msg) {
 
 // UI User actions
 newList.addEventListener("keyup", fetchNewList)
-document.addEventListener('DOMContentLoaded', () => chrome.storage.local.get(CONFIG_DEFAULT, loadOptions))
+// document.addEventListener('DOMContentLoaded', () => chrome.storage.local.get(CONFIG_DEFAULT, loadOptions))
+loadOptions().then(cfg => cfg.lists.forEach(uiAddList))
 SAVE.addEventListener('click', saveOptions)
-
-//todo: other stuff, organize
-const saveOptions = () => {
-    console.log("Saving config")
-    setStatus("Settings saved")
-    // send update event
-    return  
-    chrome.storage.local.set(
-      { lists: [] },
-      () => {
-      }
-    );
-  };
-  
