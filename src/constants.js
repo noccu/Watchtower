aca2844dc459961237bff1b43389bef8b8082500
noccu <37862870+noccu@users.noreply.github.com>
2024-04-19 11:58:12 +0200

    Use TS typings

--- src/constants.js
+++ src/constants.js
@@ -9,23 +9,3 @@ export const PLATFORMS = {
 export const CONFIG_DEFAULT = {
     lists: []
 }
-
-/**
- * A platform user part of a list.
- * @typedef {Object} User
- * @property {string} user - Platform username/handle. Required.
- * @property {string} id - Platform ID of the user, generally used as main key.
- * @property {List[]} [onLists] - Present when cached, contains refs to lists the user is on.
- */
-
-/**
- * A list following a specific theme. Only main properties listed.
- * @typedef {Object} List
- * @property {string} name - List name.
- * @property {string} label - The label to display on posts.
- * @property {string} msg - The message to display on profiles0
- * @property {string} color - The color to use for list UI elements.
- * @property {string} homepage - The main/support page of the list.
- * @property {string} source - Download/update URL of the list.
- * @property {Object.<string, User[]>} users - Users on the list, split by platform (key).
- */
---
+++ src/typings/types.d.ts
@@ -0,0 +1,27 @@
+/** A platform user part of a list. */
+interface User {
+    /** Platform username/handle. Required. */
+    user: string
+    /** Platform ID of the user, generally used as main key. */
+    id: string
+    /** Present when cached, contains refs to lists the user is on. */
+    onLists?: List[]
+}
+
+/** A list following a specific theme. Only main properties listed. */
+interface List {
+    /** List name. */
+    name: string
+    /** The label to display on posts. */
+    label: string
+    /** The message to display on profiles0 */
+    msg: string
+    /** The color to use for list UI elements. */
+    color: string
+    /** The main/support page of the list. */
+    homepage: string
+    /** Download/update URL of the list. */
+    source: string
+    /** Users on the list, split by platform (key). */
+    users: Object.<string, User[]>
+}
